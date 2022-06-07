c57cc768fc0e064311f9f611dfb657a0cff464b0
Wes <76014409+Redtigercod4@users.noreply.github.com>
2022-06-07 20:18:24 +0100

    Basic Container layout

---
+++ container-app/src/pages/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './app.css';
+
+const app = () => {
+  return (
+      <div>
+        <div className='title'>
+          <h1>React MicroFrontend POC</h1>
+          </div>
+          <hr></hr>
+      </div>
+  );
+};
+
+export default app;
